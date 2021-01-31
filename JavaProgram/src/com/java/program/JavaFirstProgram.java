adb4736d8c2fc83084d7736a9facb5b58a71070a
ss <ss@DESKTOP-8PQ5ARJ>
2021-01-31 13:54:07 +0530

    javafirstprogram

---
+++ JavaProgram/src/com/java/program/JavaFirstProgram.java
@@ -0,0 +1,12 @@
+package com.java.program;
+
+public class JavaFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
